cecb08f770bf5d48b51c3e27df9881e515e504ee
Demola <ademoladebayo@gmail.com>
2022-05-19 21:40:13 +0100

    testing:Testing CUSTOM actions

--- .github/actions/job-summaries/dist/index.js
+++ .github/actions/job-summaries/dist/index.js
@@ -8872,6 +8872,7 @@ const run = async () => {
     const owner = core.getInput("owner", { required: true });
     const repo = core.getInput("repo", { required: true });
     const run_id = core.getInput("run_id", { required: true });
+    const pr_number = core.getInput("pr_number", { required: true });
     const token = core.getInput("token", { required: true });
 
     const octokit =  github.getOctokit(token);
@@ -8882,16 +8883,27 @@ const run = async () => {
       run_id
     });
 
-    const { jobs, total_count } = listJobs;
+    const { jobs } = listJobs;
     console.log("JOBS =>", JSON.stringify(jobs, null, "\t"));
     jobs
     .map(s => s.id)
     .forEach(async (job_id) => {
-      await octokit.rest.actions.getJobForWorkflowRun({
+      const { 
+        name, 
+        status, 
+        conclusion, 
+        started_at, 
+        completed_at } = await octokit.rest.actions.getJobForWorkflowRun({
         owner,
         repo,
         job_id,
       });
+
+      console.log("NAME =>", name);
+      console.log("STATUS =>", status);
+      console.log("CONCLUSION =>", conclusion);
+      console.log("STARTED_AT =>", started_at);
+      console.log("COMPLETED_AT =>", completed_at);
     })
   } catch (error) {
     core.setFailed(error.message);
